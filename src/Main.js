a694604018d55beb73138bcb1956e218fb209bdc
Qadree Trimble <qadree_t@yahoo.com>
2021-03-23 17:54:31 -0700

    completed react app

---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from 'react'; 
+import HornedBeasts from './HornedBeasts';
+
+class Main extends React.Component {
+    render () {
+        return (
+            <main>
+                <HornedBeasts 
+                title='UniWhal' 
+                imageUrl='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+                description='A unicorn and a narwhal nuzzling their horns'
+                />
+                <HornedBeasts 
+                title='Unicorn Head' 
+                imageUrl='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'
+                description='Someone wearing a creepy unicorn head mask'
+                />
+            </main>
+        )
+    }
+}
+
+export default Main;
